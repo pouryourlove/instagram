f1f925f11c73a59662d48ff976d442509d0f63a3
Juyeon Lee <lqu134@naver.com>
2022-06-19 07:06:35 +0900

    no more error for secret!

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,10 +6,11 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      secret: process.env.SECRET,
     }),
     // ...add more providers here
   ],
+  secret:
+    "4da55b237dae0e796382e44e656566f54f0a056f2bcb50e802c8eb605302a029d878913570800dd3dd81495e9065a8515ee2712a3436613511df3298dbc676f1",
   pages: {
     signIn: "/auth/signin",
   },
